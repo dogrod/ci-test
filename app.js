45a695db7eed9b278a1cd67334930c88201cd415
dogrod <rodrickjue@hotmail.com>
2017-11-24 13:22:47 +0800

    :sparkles: Print request headers & body

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let express = require('express')
 let app = express()
 
 app.get('/', (req, res) => {
-  res.send(`Hello World. Request Info: ${req}`)
+  res.send(`Hello World. Request Info: ${req.headers} ${req.body}`)
 })
 
 app.listen(4000)
