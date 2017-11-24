161b9b8092e6aa050be2e1b9cd132c4b150f06f7
dogrod <rodrickjue@hotmail.com>
2017-11-24 13:24:16 +0800

    :sparkles: Stringify request headers

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let express = require('express')
 let app = express()
 
 app.get('/', (req, res) => {
-  res.send(`Hello World. Request Info: ${req.headers} ${req.body}`)
+  res.send(`Hello World. Request Info: ${JSON.stringify(req.headers)}`)
 })
 
 app.listen(4000)
